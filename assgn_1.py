2fe3ef0e681c83361f095cecd18a9373cf5aeff8
Dominik Huffield <Dom@Doms-MacBook.lan>
2020-05-09 07:22:41 -0600

    completed

--- assgn_1.py
+++ assgn_1.py
@@ -1,2 +1,36 @@
-#some python code
+#!/usr/bin/python
+
+class MaxSizeList(object):
+    def __init__(self, max):
+        self.max = max
+        self.list = []
+
+    def push(self, obj):
+        self.list.append(obj)
+        if len(self.list) > self.max:
+            self.list.pop(0)
+
+    def get_list(self):
+        return self.list
+
+
+a = MaxSizeList(3)
+b = MaxSizeList(1)
+
+a.push("hey")
+a.push("hi")
+a.push("let's")
+a.push("go")
+
+b.push("hey")
+b.push("hi")
+b.push("let's")
+b.push("go")
+
+print(a.get_list())
+print(b.get_list())
+
+
+
+
 
